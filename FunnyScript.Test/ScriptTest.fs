0d937b7931d8a17e52bf90d11560532aa761b889
u1roh <terada.yuichiro+github@gmail.com>
2017-03-25 15:06:09 +0900

    テストコードのリファクタリング

--- FunnyScript.Test/ScriptTest.fs
+++ FunnyScript.Test/ScriptTest.fs
@@ -9,72 +9,31 @@ open FunnyScript
   test "do trace 10; do trace 20;"
   *)
 
-let scriptAssertEquals expected script =
+let (==>) script expected =
   script
   |> Script.runScriptStr
   |> assertEquals (Some expected)
 
+
 let testScript = test "scripting test" {
-  do!
-    "1 + 2 * (3 + 4)"
-    |> Script.runScriptStr
-    |> assertEquals (Some (Int 15))
-  do!
-    "a := 1 + 2; b := 3 + 4; a * b"
-    |> Script.runScriptStr
-    |> assertEquals (Some (Int 21))
-  do!
-    "f := a -> a + 1; f 2"
-    |> Script.runScriptStr
-    |> assertEquals (Some (Int 3))
-  do!
-    "f := a -> b -> a * b; f 3 4"
-    |> Script.runScriptStr
-    |> assertEquals (Some (Int 12))
-  do!
-    "true ? 1 : 2"
-    |> Script.runScriptStr
-    |> assertEquals (Some (Int 1))
-  do!
-    "a := 5; a < 6 ? 1 : 2"
-    |> Script.runScriptStr
-    |> assertEquals (Some (Int 1))
-  do!
-    "fac := n -> n == 0 ? 1 : n * fac (n - 1); fac 4"
-    |> Script.runScriptStr
-    |> assertEquals (Some (Int 24))
-  do!
-    "r := { a := 10; b := 2 + 3; }; r.b"
-    |> Script.runScriptStr
-    |> assertEquals (Some (Int 5))
-  do!
-    "r := { f := n -> n * 2; }; r.f 2"
-    |> Script.runScriptStr
-    |> assertEquals (Some (Int 4))
-  do!
-    "r := { f := n -> n * 2; g := n -> f (n + 1); }; (r.g 2)"
-    |> Script.runScriptStr
-    |> assertEquals (Some (Int 6))
-  do!
-    "f := x -> y -> x + y; 10 |> f 5"
-    |> Script.runScriptStr
-    |> assertEquals (Some (Int 15))
-  do!
-    "[1, 2, 3 + 4]"
-    |> Script.runScriptStr
-    |> assertEquals (Some (List (FunnyList.ofArray [| Int 1; Int 2; Int 7 |])))
+  do! "1 + 2 * (3 + 4)" ==> (Int 15)
+  do! "a := 1 + 2; b := 3 + 4; a * b" ==> (Int 21)
+  do! "f := a -> a + 1; f 2" ==> (Int 3)
+  do! "f := a -> b -> a * b; f 3 4" ==> (Int 12)
+  do! "true ? 1 : 2" ==> (Int 1)
+  do! "a := 5; a < 6 ? 1 : 2" ==> (Int 1)
+  do! "fac := n -> n == 0 ? 1 : n * fac (n - 1); fac 4" ==> Int 24
+  do! "r := { a := 10; b := 2 + 3; }; r.b" ==> Int 5
+  do! "r := { f := n -> n * 2; }; r.f 2" ==> Int 4
+  do! "r := { f := n -> n * 2; g := n -> f (n + 1); }; (r.g 2)" ==> Int 6
+  do! "f := x -> y -> x + y; 10 |> f 5" ==> Int 15
+  do! "[1, 2, 3 + 4]" ==> List (FunnyList.ofArray [| Int 1; Int 2; Int 7 |])
 }
 
 let typeTest = test "type test" {
-  do!
-    "typeof 1" |> Script.runScriptStr
-    |> assertEquals (Some (Type IntType))
-  do!
-    "typeof 1.0" |> Script.runScriptStr
-    |> assertEquals (Some (Type FloatType))
-  do!
-    "typeof (x -> x * x)" |> Script.runScriptStr
-    |> assertEquals (Some (Type FuncType))
-  do! "typeof true" |> scriptAssertEquals (Type BoolType)
-  do! "typeof true == bool" |> scriptAssertEquals True
+  do! "typeof 1" ==> Type IntType
+  do! "typeof 1.0" ==> Type FloatType
+  do! "typeof (x -> x * x)" ==> Type FuncType
+  do! "typeof true" ==> Type BoolType
+  do! "typeof true == bool" ==> True
 }
