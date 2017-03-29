02791c321153ea171bf074e4da62ab99f41aa87b
u1roh <terada.yuichiro+github@gmail.com>
2017-03-29 21:57:47 +0900

    test01.txt に iter 関数を書いてみた

--- FunnyScript.Console/Program.fs
+++ FunnyScript.Console/Program.fs
@@ -1,10 +1,21 @@
 ﻿open FunnyScript
+open System
 
 [<EntryPoint>]
 let main argv = 
-  "fac := n -> n == 0 ? 1 : n * fac (n - 1);" +
-  "do fac 4 |> trace;"
-  |> Script.runScriptStr
-  |> printfn "%A"
-
+  if argv.Length = 0 then
+    "fac := n -> n == 0 ? 1 : n * fac (n - 1);" +
+    "do fac 4 |> trace;"
+    |> Script.runScriptStr
+    |> printfn "%A"
+  else
+    printfn "%s:" argv.[0]
+    let script = IO.File.ReadAllText (IO.Path.Combine (@"..\..\", argv.[0]))
+    printfn "%s" script
+    printfn "---"
+    script
+    |> Script.runScriptStr
+    |> printfn "%A"
+   
+  Console.ReadLine() |> ignore 
   0 // 整数の終了コードを返します
