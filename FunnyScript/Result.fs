6343d2f32b351e13eff51a928d68b1600f4bf024
u1roh <terada.yuichiro+github@gmail.com>
2017-04-03 23:17:39 +0900

    F# 4.1 で標準になる Result 型をコピペ

---
+++ FunnyScript/Result.fs
@@ -0,0 +1,18 @@
+﻿namespace FunnyScript
+// --- F# 4.1 で標準となる Result 型 ---
+
+type Result<'T,'TError> =
+    | Ok of 'T 
+    | Error of 'TError
+
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module Result =
+
+  [<CompiledName("Map")>]
+  let map f inp = match inp with Error e -> Error e | Ok x -> Ok (f x)
+
+  [<CompiledName("MapError")>]
+  let mapError f inp = match inp with Error e -> Error (f e) | Ok x -> Ok x
+
+  [<CompiledName("Bind")>]
+  let bind f inp = match inp with Error e -> Error e | Ok x -> f x
