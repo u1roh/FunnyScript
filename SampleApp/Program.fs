322af74a784e70f327201368cbdfa659b17cff70
u1roh <terada.yuichiro@gmail.com>
2017-08-15 23:05:17 +0900

    スクリプトで描画する簡単なサンプルアプリを試作

--- SampleApp/Program.fs
+++ SampleApp/Program.fs
@@ -1,24 +1,42 @@
 ﻿open System;
+open System.Drawing;
+open System.Windows.Forms;
 open FunnyScript;
 
-let script =
-  """
-  console := System.Console;
-  console.WriteLine "funny"
-  """
+type ScriptRunner () =
+  let env =
+    Script.defaultEnv
+    |> CLR.loadAssembly (typeof<System.Drawing.Graphics>.Assembly)
+  let mutable script = ""
+  let mutable program : Expr = { Value = Obj Null; Position = None }
+
+  member __.Script = script
+
+  member __.Parse str =
+    script <- str
+    match Parser.parse "" script with
+    | Error e -> printfn "%s" e
+    | Ok expr -> program <- expr
+  
+  member __.Run (argname, argobj) =
+    let env = env |> Map.add (Name argname) (ClrObj argobj |> Ok)
+    Script.eval env program |> printfn "%A"
 
 [<EntryPoint>]
 let main argv = 
-  match Parser.parse "" script with
-  | Error e -> printfn "%s" e
-  | Ok expr ->
-    let timer = new System.Timers.Timer()
-    let ev = timer.GetType().GetEvent "Elapsed"
-    //ev.AddEventHandler(timer, Timers.ElapsedEventHandler(fun sender e -> printfn "."))
-    //let handler = Delegate.CreateDelegate (ev.EventHandlerType, (fun sender e -> ()))
-//    ev.AddEventHandler(timer, (fun sender e -> ()))
-    timer.Elapsed.Add (fun e ->
-      Script.eval Script.defaultEnv expr |> ignore)
-    timer.Start()
-  stdin.ReadLine() |> ignore
+  let sampleScript = """d := System.Drawing;
+pen := d.Pen.new (d.Color.Black);
+do g.DrawEllipse (pen, 10, 10, 200, 100); """
+  let runner = ScriptRunner()
+  runner.Parse sampleScript
+  let mainform = new Form (Width = 600, Height = 600)
+  let editor = new TextBox (Multiline = true, Dock = DockStyle.Bottom, Height = 200, ScrollBars = ScrollBars.Vertical, Font = new Font("Consolas", 11.0f))
+  editor.Text <- runner.Script
+  editor.KeyDown.Add (fun e ->
+    if e.KeyCode = Keys.F5 then
+      runner.Parse editor.Text
+      mainform.Invalidate())
+  mainform.Controls.Add editor
+  mainform.Paint.Add (fun e -> runner.Run ("g", e.Graphics))
+  Application.Run mainform
   0
