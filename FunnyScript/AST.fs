3963838315c2b6acc69fcc281f57e2f78394a23a
u1roh <terada.yuichiro@gmail.com>
2017-09-23 23:49:06 +0900

    リファクタリング

--- FunnyScript/AST.fs
+++ FunnyScript/AST.fs
@@ -1,27 +1,6 @@
 ﻿[<AutoOpen>]
 module FunnyScript.AST
 
-type Position = {
-    FilePath  : string
-    LineCol1  : int * int
-    LineCol2  : int * int
-  } with
-  override this.ToString () =
-    sprintf "%s %A~%A" this.FilePath this.LineCol1 this.LineCol2
-
-type IFunnyArray =
-  inherit System.Collections.IEnumerable
-  abstract Length : int
-  abstract Item : int -> obj with get
-
-type IntInterval = { min : int; max : int } with
-  member this.contains n = this.min <= n && n <= this.max
-  interface IFunnyArray with
-    member this.Length = this.max - this.min + 1 |> max 0
-    member this.Item with get i = this.min + i |> box
-    member this.GetEnumerator () = (seq { this.min .. this.max } :> System.Collections.IEnumerable).GetEnumerator()
-
-
 type Env = Map<string, Result>
 
 and Err =
@@ -45,9 +24,6 @@ and ErrInfo = {
 
 and Result = Result<obj, ErrInfo>
 
-and IMutable =
-  abstract Value : obj with get, set
-
 and IFuncObj =
   abstract Apply : obj -> Result
 
@@ -55,8 +31,6 @@ and Instance = {
     Data : obj
     Type : FunnyType
   }
-
-and Record = Map<string, obj>
 
 and Expr =
   | Obj of obj
---
+++ FunnyScript/Data.fs
@@ -0,0 +1,51 @@
+﻿namespace FunnyScript
+open System
+
+
+type Position = {
+    FilePath  : string
+    LineCol1  : int * int
+    LineCol2  : int * int
+  } with
+  override this.ToString () =
+    sprintf "%s %A~%A" this.FilePath this.LineCol1 this.LineCol2
+
+type IFunnyArray =
+  inherit System.Collections.IEnumerable
+  abstract Length : int
+  abstract Item : int -> obj with get
+
+type IntInterval = { min : int; max : int } with
+  member this.contains n = this.min <= n && n <= this.max
+  interface IFunnyArray with
+    member this.Length = this.max - this.min + 1 |> max 0
+    member this.Item with get i = this.min + i |> box
+    member this.GetEnumerator () = (seq { this.min .. this.max } :> System.Collections.IEnumerable).GetEnumerator()
+
+
+type IMutable =
+  abstract Value : obj with get, set
+
+type Record = Map<string, obj>
+
+// 下記はまだ迷っているのでコメントアウト
+
+//type Result<'TError> = Result<obj, 'TError>
+//
+//type IFuncObj<'E> =
+//  abstract Apply : obj -> Result<'E>
+//
+//and TypeId<'E> =
+//  | UserType of name:string * ctor:IFuncObj<'E> * members:Map<string, IFuncObj<'E>>
+//  | ClrType  of System.Type
+//
+//and FunnyType<'E> = {
+//    Id : TypeId<'E>
+//    mutable ExtMembers : Map<string, IFuncObj<'E>>
+//  }
+//
+//type Instance<'E> = {
+//    Data : obj
+//    Type : FunnyType<'E>
+//  }
+
